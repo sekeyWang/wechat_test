e5b6aaed91905bd373307efae64eeaa08ae80945
sekeyWang <0731wsk@163.com>
2017-10-25 12:03:31 +0800

    add_code
    add initial code

---
+++ handle.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# filename: handle.py
+import hashlib
+import reply
+import receive
+import web
+
+class Handle(object):
+    def POST(self):
+        try:
+            webData = web.data()
+            print "Handle Post webdata is ", webData   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = recMsg.
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
+            else:
+                print "暂且不处理"
+                return "success"
+        except Exception, Argment:
+            return Argment
+#测试一下修改功能
